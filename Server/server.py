ff132df88cb8b80a64c0700d282cc4a3b36bd7f9
Aiden-Ziegelaar <aiden335@gmail.com>
2016-05-08 00:15:00 +0800

    added ssl

--- Server/server.py
+++ Server/server.py
@@ -1,25 +1,35 @@
 import socket
 import config
 import sys
+from OpenSSL import SSL
+import os
 
 # connect socket
 def runserver(port, data):
     config.init()
+
+    root    = os.getcwd()
+    ctx     = SSL.Context(SSL.TLSv1_2_METHOD)
+    ctx.use_privatekey_file(os.path.join(root, 'server.pkey'))
+    ctx.use_certificate_file(os.path.join(root, 'server.cert'))
+
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    encrypted_conn = SSL.Connection(ctx, conn)
     host = socket.gethostname()
     try:
-        conn.bind(("192.168.43.70", int(port)))
+        encrypted_conn.bind(("", int(port)))
     except socket.error as msg:
         print('Bind failed.')
         sys.exit()
     conn.listen(config.backlog)
     print("Server running on: \nHost: " + host + "\nPort: " + port)
-    receive(conn, data)
+    receive(encrypted_conn, data)
 
 # send data
 def receive(conn, data):
     while 1==1:
         client, addr = conn.accept()
+        conn.do_handshake()
         payload = client.recv(config.payload_size).decode()
         if payload:
             print(str(payload).strip())
