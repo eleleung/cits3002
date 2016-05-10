9a61cc6471aacc5619b4782628b846fb8f86a006
Aiden-Ziegelaar <aiden335@gmail.com>
2016-05-10 15:39:32 +0800

    added error checks and files

--- Server/config.py
+++ Server/config.py
@@ -1,5 +1,8 @@
+import os
 def init():
     global backlog
     backlog = 5
     global payload_size
     payload_size = 1024
+    global root
+    root = os.getcwd()
--- Server/server.py
+++ Server/server.py
@@ -4,26 +4,62 @@ import sys
 from OpenSSL import SSL
 import os
 
-# connect socket
-def runserver(port, data):
-    config.init()
-
-    root    = os.getcwd()
+def create_context():
     ctx     = SSL.Context(SSL.TLSv1_2_METHOD)
-    ctx.use_privatekey_file(os.path.join(root, 'server.pkey'))
-    ctx.use_certificate_file(os.path.join(root, 'server.cert'))
+    try:
+        ctx.use_privatekey_file(os.path.join(config.root, 'server.pkey'))
+    except SSL.Error as error:
+        print('server.pkey could not be found \nPlace in directory: ' + str(config.root) + "\n", file=sys.stderr)
+        try:
+            ctx.use_certificate_file(os.path.join(config.root, 'server.cert'))
+        except SSL.Error as error:
+            print('server.cert could not be found \nPlace in directory: ' + str(config.root) + "\n", file=sys.stderr)
+            sys.exit()
+    try:
+        ctx.use_certificate_file(os.path.join(config.root, 'server.cert'))
+    except SSL.Error as error:
+        print('server.cert could not be found \nPlace in directory: ' + str(config.root) + "\n", file=sys.stderr)
+        sys.exit()
+    return ctx
 
+# connect socket
+def runserver(port, file):
+    config.init()
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ctx = create_context()
     encrypted_conn = SSL.Connection(ctx, conn)
     host = socket.gethostname()
     try:
         encrypted_conn.bind(("", int(port)))
     except socket.error as msg:
-        print('Bind failed.')
+        print('Bind failed, socket error message: ' + str(msg))
         sys.exit()
     conn.listen(config.backlog)
     print("Server running on: \nHost: " + host + "\nPort: " + port)
-    receive(encrypted_conn, data)
+    receive(encrypted_conn, file)
+
+def send_file(conn, file):
+    client, addr = conn.accept()
+    conn.do_handshake()
+    filename = client.recv(config.payload_size).decode()
+    send_file = open(filename, "r")
+    data = send_file.read(config.payload_size)
+    while data:
+        client.send(data)
+        data = send_file.read(config.payload_size)
+    sendfile.close()
+
+def receive_file(conn):
+    client, addr = conn.accept()
+    conn.do_handshake()
+    filename = client.recv(config.payload_size).decode()
+    recv_file = open(filename, "w+")
+    data = client.recv(config.payload_size)
+    while data:
+        recv_file.write(data)
+        data = client.recv(config.payload_size)
+    recv_file.close()
+
 
 # send data
 def receive(conn, data):
@@ -32,7 +68,7 @@ def receive(conn, data):
         conn.do_handshake()
         payload = client.recv(config.payload_size).decode()
         if payload:
-            print(str(payload).strip())
+            print(str(payload))
         client.send(bytes(data, "UTF-8"))
         client.close()
 
